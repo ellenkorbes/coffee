bf37f0d631b6a1308d5ffbae3cb8a56745b81553
Ellen Körbes <ellenkorbes@gmail.com>
2019-01-28 22:52:01 +0100

    soymilk

--- coffee.go
+++ coffee.go
@@ -1,13 +1,13 @@
 package coffee
 
-// 1.1.0-espresso
-// 1.2.0-espresso
-// 1.3.0-espresso
+// 1.1.0-soymilk
+// 1.2.0-soymilk
+// 1.3.0-soymilk
 // 1.4.0-soymilk
 // 1.5.0-decaf
 // 1.6.0-iced
 
 // Cup is a unit of coffee.
 func Cup() string {
-	return "espresso"
+	return "soymilk"
 }
