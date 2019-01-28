50ac2b7ea97bcd3aff3a4870e9cb8c768afcd5e8
Ellen Körbes <ellenkorbes@gmail.com>
2019-01-28 21:25:21 +0100

    coffee

---
+++ coffee.go
@@ -0,0 +1,13 @@
+package coffee
+
+// 1.1.0-percolated
+// 1.2.0-cappuccino
+// 1.3.0-espresso
+// 1.4.0-soymilk
+// 1.5.0-decaf
+// 1.6.0-iced
+
+// Cup is a unit of coffee.
+func Cup() string {
+	return "percolated"
+}
